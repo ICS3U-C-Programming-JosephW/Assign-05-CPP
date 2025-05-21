d6a999172a1db2982a31879c9ba0c0d38cdef570
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-05-20 20:17:31 -0400

    Fixed header comments and renamed template.cpp to frictionCalc.cpp.

---
+++ frictionCalc.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025. Joseph Wondimagnehu. All rights reserved.
+//
+// Created by: Joseph Wondimagnehu
+// Date: May 20, 2025
+/* This program calculates the static or
+kinetic friction given the friction type,
+coefficient of friction, and normal force. */
+
+/* Include the iostream library for
+input and output functionalities. */
+#include <iostream>
+
+// Runs the main function.
+int main() {
+    
+}
